40e6f5746a56ea60e0d04c6ad8d9967b937115f3
matthewskillington <matthew.skillington@ideagenplc.com>
2021-02-10 15:16:37 +0000

    Unit tests and cleanup

--- src/app/photo-viewer/photo-viewer.component.spec.ts
+++ src/app/photo-viewer/photo-viewer.component.spec.ts
@@ -1,10 +1,14 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { PageEvent } from '@angular/material/paginator';
 import { PhotoViewerComponent } from './photo-viewer.component';
+import { PhotoViewerService } from './photo-viewer.service';
 
 describe('PhotoViewerComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PhotoViewerComponent],
+      providers: [HttpHandler, HttpClient, PhotoViewerService],
     }).compileComponents();
   });
 
@@ -15,8 +19,88 @@ describe('PhotoViewerComponent', () => {
   });
 
   it(`should have top message 'Welcome to the Photo Viewer!'`, () => {
+    //Arrange:
     const fixture = TestBed.createComponent(PhotoViewerComponent);
     const app = fixture.componentInstance;
+
+    //Assert:
     expect(app.topMessage).toEqual('Welcome to the Photo Viewer!');
+  });
+
+  it('should update low and high values when GetPaginatorData called', () => {
+    //Arrange:
+    const fixture = TestBed.createComponent(PhotoViewerComponent);
+    const app = fixture.componentInstance;
+    const pageEvent = new PageEvent();
+    pageEvent.pageIndex = 1;
+    pageEvent.pageSize = 10;
+
+    //Act:
+    app.getPaginatorData(pageEvent);
+
+    //Assert:
+    expect(app.lowValue).toEqual(10);
+    expect(app.highValue).toEqual(20);
+  });
+
+  it('should show more items when pagination items changed', () => {
+    //Arrange:
+    const fixture = TestBed.createComponent(PhotoViewerComponent);
+    const app = fixture.componentInstance;
+    const pageEvent: PageEvent = {
+      pageIndex: 0,
+      pageSize: 25,
+      length: 500,
+    };
+
+    //Act:
+    app.getPaginatorData(pageEvent);
+    expect(app.lowValue).toEqual(0);
+    expect(app.highValue).toEqual(25);
+
+    const pageEvent2 = new PageEvent();
+    pageEvent2.pageIndex = 0;
+    pageEvent.pageSize = 100;
+    app.getPaginatorData(pageEvent);
+
+    //Assert:
+    expect(app.lowValue).toEqual(0);
+    expect(app.highValue).toEqual(100);
+  });
+
+  it('should arrange photos array when FilterByTitle called', () => {
+    //Arrange:
+    const fixture = TestBed.createComponent(PhotoViewerComponent);
+    const app = fixture.componentInstance;
+    app.photos = [
+      {
+        albumId: 1,
+        id: 1,
+        url: '',
+        thumbnailUrl: '',
+        title: 'C title',
+      },
+      {
+        albumId: 1,
+        id: 2,
+        url: '',
+        thumbnailUrl: '',
+        title: 'A title',
+      },
+      {
+        albumId: 1,
+        id: 3,
+        url: '',
+        thumbnailUrl: '',
+        title: 'B title',
+      },
+    ];
+
+    //Act:
+    app.FilterByTitle();
+
+    //Assert:
+    expect(app.photos[0].title).toEqual('A title');
+    expect(app.photos[2].title).toEqual('C title');
   });
 });
